90aad0e8afb1b5c92e66367df2d3d78121ad4537
BartoszP07 <bartoszp0980@gmail.com>
2026-05-01 22:37:58 +0200

    0

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 # Import modules
-from tkinter import *
-import tkinter as ttk
-import screeninfo
+import pygame, sys, time
 
 class app:
     def __init__(self):
-        self.set_window()
+        self.screen = pygame.display.set_mode((500, 500))
+        pygame.display.set_caption("Revision")
+        self.clock = pygame.time.Clock()
+        self.delta_time = 0
         
-    def set_window(self):
-        # Get the resolution of the current screen
-        windowW = screeninfo.screeninfo.get_monitors()[0].width
-        windowH = screeninfo.screeninfo.get_monitors()[0].height
-        self.root = Tk()
-        self.root.title("revision")
-        self.root.geometry(f"{windowW}x{windowH}")
-        self.frame = ttk.Frame(self.root)
+    def quit_game(self):
+        pygame.quit()
+        sys.exit()
+        
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit_game()
+    
+    def update(self):
+        pass
+    
+    def draw(self):
+        self.screen.fill((25, 25, 25))
         
     def run(self):
-        self.root.mainloop()
+        while True:
+            self.delta_time = self.clock.tick(60) / 1000
+            
+            self.events()
+            self.update()
+            self.draw()
+            
+            pygame.display.flip()
+        
     
 if __name__ == "__main__":
     a = app()
